79f5e6839c8a52d406258521e087367e53c0db7d
Jaydeep Nayak <jaydeepnayak23@gmail.com>
2021-08-14 21:42:53 +0530

    REmoved some lines

--- src/Jaydeep/OOPs/Main.java
+++ src/Jaydeep/OOPs/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Jaron jv = new Jaron();
         Jaron jq = new Jaron("Lola","Solla");
-        System.out.println("Hello Jaydeep");
         System.out.println("Hello Jaydeep2");
         System.out.println("Hello Ashlan");
     }
