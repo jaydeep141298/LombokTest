200615d7ea3cbae31c414a5b11d6d69e72463446
Jaydeep Nayak <jaydeepnayak23@gmail.com>
2021-08-14 21:06:18 +0530

    My New Commit

---
+++ src/Jaydeep/OOPs/Jaron.java
@@ -0,0 +1,31 @@
+package Jaydeep.OOPs;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.sql.SQLOutput;
+import java.util.logging.Logger;
+
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Log4j2
+public class Jaron {
+
+  //  Logger log = Logger.getLogger();
+    @NonNull
+    public String a1;
+    public int a2;
+
+    @NonNull
+    String a3;
+
+    public void hasna(){
+        System.out.println("Has raha hu");
+    }
+
+
+}
---
+++ src/Jaydeep/OOPs/Main.java
@@ -0,0 +1,11 @@
+package Jaydeep.OOPs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Jaron jv = new Jaron();
+        Jaron jq = new Jaron("Lola","Solla");
+        System.out.println("Hello Jaydeep");
+
+    }
+}
